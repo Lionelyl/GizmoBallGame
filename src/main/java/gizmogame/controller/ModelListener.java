f9d83fe9e886b4fbc68e3b3648be327bda4fbe29
lionel_xyl <1103683657@qq.com>
2019-11-29 21:28:31 +0800

    second commit

---
+++ src/main/java/gizmogame/controller/ModelListener.java
@@ -0,0 +1,30 @@
+package gizmogame.controller;
+
+import gizmogame.view.BoardView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+
+public class ModelListener implements ActionListener {
+
+    private BoardView boardView;
+
+    public ModelListener(BoardView boardView) {
+        this.boardView = boardView;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //System.out.println(e.getActionCommand());
+        if(e.getActionCommand().equals("PLAY")){
+            boardView.getTimer().start();
+            //System.out.println("timer start");
+        }
+        else{
+            boardView.getTimer().stop();
+            //System.out.println("timer stop");
+        }
+
+    }
+}
